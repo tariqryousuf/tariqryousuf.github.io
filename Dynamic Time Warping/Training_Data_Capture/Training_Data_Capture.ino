8f12e6fa035691914333308a71c2da8fa123ccdb
Tariq Yousuf <tariqryanyousuf@gmail.com>
2020-11-12 12:26:51 -0800

    Create Training_Data_Capture.ino

---
+++ Dynamic Time Warping/Training_Data_Capture/Training_Data_Capture.ino
@@ -0,0 +1,101 @@
+/*
+  IMU Capture
+  This example uses the on-board IMU to start reading acceleration and gyroscope
+  data from on-board IMU and prints it to the Serial Monitor for one second
+  when the significant motion is detected.
+  You can also use the Serial Plotter to graph the data.
+  The circuit:
+  - Arduino Nano 33 BLE or Arduino Nano 33 BLE Sense board.
+  Created by Don Coleman, Sandeep Mistry
+  Modified by Dominic Pajak, Sandeep Mistry
+  This example code is in the public domain.
+*/
+
+#include <Arduino_LSM9DS1.h>
+
+const float accelerationThreshold = 2.5; // threshold of significant in G's
+const int numSamples = 119;
+
+int samplesRead = numSamples;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  // print the header
+  Serial.println("aX,aY,aZ");
+}
+
+void loop() {
+  float aX, aY, aZ;
+
+  // wait for significant motion
+  while (samplesRead == numSamples) {
+    if (IMU.accelerationAvailable()) {
+      // read the acceleration data
+      IMU.readAcceleration(aX, aY, aZ);
+
+      // sum up the absolutes
+      float aSum = fabs(aX) + fabs(aY) + fabs(aZ);
+
+      // check if it's above the threshold
+      if (aSum >= accelerationThreshold) {
+        // reset the sample read count
+        samplesRead = 0;
+        break;
+      }
+    }
+  }
+
+  // check if the all the required samples have been read since
+  // the last time the significant motion was detected
+  while (samplesRead < numSamples) {
+    // check if both new acceleration and gyroscope data is
+    // available
+    if (IMU.accelerationAvailable()) {
+      // read the acceleration and gyroscope data
+      IMU.readAcceleration(aX, aY, aZ);
+
+      samplesRead++;
+
+      int ax = non_linear_quantization(aX);
+      int ay = non_linear_quantization(aY);
+      int az = non_linear_quantization(aZ);
+
+      // print the data in CSV format
+      Serial.print(ax);
+      Serial.print(',');
+      Serial.print(ay);
+      Serial.print(',');
+      Serial.print(az);
+      
+   
+      Serial.println();
+
+      if (samplesRead == numSamples) {
+        // add an empty line if it's the last sample
+        Serial.println();
+      }
+    }
+  }
+}
+
+int non_linear_quantization( float a )
+{
+  int return_value;
+  if (abs(a) < 1)
+    return_value = (int) (a * 10.0);
+  else if (abs(a) < 2)
+    return_value = 10 + (int) ((a - 1.0) * 2.0);
+  else
+    return_value = 16;
+
+  return return_value;
+  
+}
+   
